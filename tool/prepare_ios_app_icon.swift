9a2aa8f1014fb7fdeb94a6271aa1bff5d6322aef
jetkizdigital-droid <jetkizdigital@gmail.com>
2026-09-16 17:12:04 +0600

    fix(ios): render opaque App Store icon with CoreGraphics

--- tool/prepare_ios_app_icon.swift
+++ tool/prepare_ios_app_icon.swift
@@ -1,4 +1,5 @@
 import AppKit
+import CoreGraphics
 import Foundation
 
 guard CommandLine.arguments.count == 3 else {
@@ -9,65 +10,68 @@ guard CommandLine.arguments.count == 3 else {
 let inputURL = URL(fileURLWithPath: CommandLine.arguments[1])
 let outputURL = URL(fileURLWithPath: CommandLine.arguments[2])
 
-guard let image = NSImage(contentsOf: inputURL) else {
+guard let sourceImage = NSImage(contentsOf: inputURL) else {
   fputs("Unable to read input image: \(inputURL.path)\n", stderr)
   exit(65)
 }
 
-let pixels = 1024
-guard let bitmap = NSBitmapImageRep(
-  bitmapDataPlanes: nil,
-  pixelsWide: pixels,
-  pixelsHigh: pixels,
-  bitsPerSample: 8,
-  samplesPerPixel: 3,
-  hasAlpha: false,
-  isPlanar: false,
-  colorSpaceName: .deviceRGB,
-  bytesPerRow: 0,
-  bitsPerPixel: 24
+var proposedRect = NSRect(origin: .zero, size: sourceImage.size)
+guard let sourceCG = sourceImage.cgImage(
+  forProposedRect: &proposedRect,
+  context: nil,
+  hints: [.interpolation: NSImageInterpolation.high]
 ) else {
-  fputs("Unable to allocate output bitmap\n", stderr)
+  fputs("Unable to decode input image\n", stderr)
+  exit(65)
+}
+
+let pixels = 1024
+let colorSpace = CGColorSpaceCreateDeviceRGB()
+let bitmapInfo = CGBitmapInfo.byteOrder32Big.rawValue |
+  CGImageAlphaInfo.noneSkipLast.rawValue
+
+guard let context = CGContext(
+  data: nil,
+  width: pixels,
+  height: pixels,
+  bitsPerComponent: 8,
+  bytesPerRow: pixels * 4,
+  space: colorSpace,
+  bitmapInfo: bitmapInfo
+) else {
+  fputs("Unable to create CoreGraphics context\n", stderr)
   exit(70)
 }
 
-NSGraphicsContext.saveGraphicsState()
-guard let context = NSGraphicsContext(bitmapImageRep: bitmap) else {
-  fputs("Unable to create graphics context\n", stderr)
-  exit(70)
-}
-NSGraphicsContext.current = context
-context.imageInterpolation = .high
+context.interpolationQuality = .high
+context.setFillColor(NSColor.white.cgColor)
+context.fill(CGRect(x: 0, y: 0, width: pixels, height: pixels))
 
-NSColor.white.setFill()
-NSRect(x: 0, y: 0, width: pixels, height: pixels).fill()
-
-let sourceSize = image.size
-guard sourceSize.width > 0, sourceSize.height > 0 else {
+let sourceWidth = CGFloat(sourceCG.width)
+let sourceHeight = CGFloat(sourceCG.height)
+guard sourceWidth > 0, sourceHeight > 0 else {
   fputs("Input image has invalid dimensions\n", stderr)
   exit(65)
 }
 
-let scale = min(CGFloat(pixels) / sourceSize.width, CGFloat(pixels) / sourceSize.height)
-let targetSize = NSSize(width: sourceSize.width * scale, height: sourceSize.height * scale)
-let targetRect = NSRect(
-  x: (CGFloat(pixels) - targetSize.width) / 2,
-  y: (CGFloat(pixels) - targetSize.height) / 2,
-  width: targetSize.width,
-  height: targetSize.height
+let scale = min(CGFloat(pixels) / sourceWidth, CGFloat(pixels) / sourceHeight)
+let targetWidth = sourceWidth * scale
+let targetHeight = sourceHeight * scale
+let targetRect = CGRect(
+  x: (CGFloat(pixels) - targetWidth) / 2,
+  y: (CGFloat(pixels) - targetHeight) / 2,
+  width: targetWidth,
+  height: targetHeight
 )
 
-image.draw(
-  in: targetRect,
-  from: .zero,
-  operation: .sourceOver,
-  fraction: 1.0,
-  respectFlipped: true,
-  hints: [.interpolation: NSImageInterpolation.high]
-)
-context.flushGraphics()
-NSGraphicsContext.restoreGraphicsState()
+context.draw(sourceCG, in: targetRect)
 
+guard let outputCG = context.makeImage() else {
+  fputs("Unable to render output image\n", stderr)
+  exit(70)
+}
+
+let bitmap = NSBitmapImageRep(cgImage: outputCG)
 guard let png = bitmap.representation(using: .png, properties: [:]) else {
   fputs("Unable to encode PNG\n", stderr)
   exit(70)
